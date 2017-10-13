44bffbf250c3756ad4d96a696627bdf4386429a8
Peggy Xia <ningweix@umich.edu>
2017-10-13 16:23:35 -0400

    lock voice auto complete UI view (disable landscape)

--- brad3/SceneViewController.swift
+++ brad3/SceneViewController.swift
@@ -24,6 +24,18 @@ class SceneViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AppUtility.lockOrientation(.portrait)
+        // Or to rotate and lock
+        // AppUtility.lockOrientation(.portrait, andRotateTo: .portrait)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Don't forget to reset when view is being removed
+        AppUtility.lockOrientation(.all)
     }
 
     override func didReceiveMemoryWarning() {
