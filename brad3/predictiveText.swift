debb84abc0a256a6bd37ac45cf470896901218d2
yifjiang <james970618@sjtu.edu.cn>
2017-10-14 15:24:17 -0400

    pre-alpha

--- brad3/predictiveText.swift
+++ brad3/predictiveText.swift
@@ -37,16 +37,20 @@ class Predictor {
     func nextWord(sentence:String) -> [String] {
         var ret:[String]=[]
         let words = sentence.lowercased().components(separatedBy: toTrimSet)
+        if words.count < 2 {return ["word1","word2","word3","word4"]}
         do {
             try dbQueue.inDatabase({ db in
-                print([words[words.endIndex-2], words.last])
+//                print([words[words.endIndex-2], words.last])
                 let rows = try Row.fetchCursor(db, "SELECT word_3, count FROM markov_model WHERE word_1 = ? AND word_2 = ? ORDER BY count DESC LIMIT 1000", arguments:[words[words.endIndex-2], words.last])
+                var i = 0
                 while let row = try rows.next(){
+                    if (i>4) {break;}
                     if let word = row["word_3"] as? String{
                         if (word=="") {continue;}
                         ret.append(word)
-                        print(word,row["count"] as Int?)
+                        print(word,(row["count"] as Int?)!)
                     }
+                    i+=1
                 }
             })
         } catch {
